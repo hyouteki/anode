6fa94cf54e900443a3da1345e479c492d1e9f7fd
Lakshay <lakshay21060@iiitd.ac.in>
2023-08-25 20:21:40 +0530

    updated video_to_frame

--- ObjectDetectionVideo/object_detection_yolov3.py
+++ ObjectDetectionVideo/object_detection_yolov3.py
@@ -1,9 +1,10 @@
 from os import system
 from os.path import abspath
 from detection import Detector
+from video_to_frame import convert_video_to_frame_dir
 
 image_path = r"assets/catdog.jpg"
-model_path = abspath(r"model\model.h5")
+model_path = abspath(r"model/model.h5")
 
 detector = Detector(model_path = model_path)
 detector.do_detect(image_path = image_path)
--- ObjectDetectionVideo/video_to_frame.py
+++ ObjectDetectionVideo/video_to_frame.py
@@ -1,6 +1,7 @@
 """
 @requirements
 opencv-python
+termcolor
 """
 
 from cv2 import VideoCapture, imwrite, destroyAllWindows
@@ -9,35 +10,33 @@ from os.path import join
 from shutil import rmtree, copyfile
 from termcolor import colored
 
-input_video_path = r"assets/cat.mp4"
-output_frames_dir = "frames"
+def convert_video_to_frame_dir(input_video_path, output_frames_dir = "frames", debug = False):
+    try:
+        rmtree(output_frames_dir, ignore_errors = False, onerror = None)
+    except:
+        pass
+    mkdir(output_frames_dir)
 
-try:
-    rmtree(output_frames_dir, ignore_errors=False, onerror=None)
-except:
-    pass
-mkdir(output_frames_dir)
+    frame_count = 0
+    video = VideoCapture(input_video_path)
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+        frame_filename = join(output_frames_dir, f"frame_{frame_count:04d}.jpg")
+        imwrite(frame_filename, frame)
+        frame_count += 1
+        if debug:
+            print(colored(f"Processed frame {frame_count}", "blue"))
 
-# Open the video file
-video = VideoCapture(input_video_path)
+    video.release()
+    destroyAllWindows()
+    if debug:
+        print(colored("Video to frames conversion complete", "green"))
 
-frame_count = 0
-
-while video.isOpened():
-    ret, frame = video.read()
-
-    if not ret:
-        break
-
-    frame_filename = join(output_frames_dir, f"frame_{frame_count:04d}.jpg")
-    imwrite(frame_filename, frame)
-
-    frame_count += 1
-
-    print(colored(f"Processed frame {frame_count}", "blue"))
-
-# Release the video capture object and close the OpenCV windows
-video.release()
-destroyAllWindows()
-
-print("Video to frames conversion complete.")
+if __name__ == "__main__":
+    convert_video_to_frame_dir(
+        input_video_path = r"assets/cat.mp4",
+        output_frames_dir = "frames",
+        debug = True
+    )
