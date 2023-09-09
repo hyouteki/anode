29eba82492a8bf3e5a4e57639ec2e3e501c00f02
Aakash391 <108621987+Aakash391@users.noreply.github.com>
2023-09-09 23:35:35 +0530

    Update server.py
    Computation at Server

--- socket_prog/server.py
+++ socket_prog/server.py
@@ -2,19 +2,12 @@ import socket
 import cv2
 import pickle
 import struct
+from threading import Thread
 from argparse import ArgumentParser
 from ultralytics import YOLO
 from json import load
-from keyboard import is_pressed
-from time import sleep
-from threading import Thread
-from math import ceil
 from cv2 import (
-    VideoCapture,
-    getTickCount,
-    getTickFrequency,
     cvtColor,
-    LINE_AA,
     COLOR_BGR2RGB,
     rectangle,
     getTextSize,
@@ -26,35 +19,28 @@ from cv2 import (
     destroyAllWindows,
 )
 
+def object_detection(frame, model):
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = model(frame_rgb, stream=True)
 
-class VideoStream:
-    """Camera object that controls video streaming from the webcam"""
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            rectangle(frame, (x1, y1), (x2, y2), color=(223, 65, 80), thickness=2)
+            confidence = int(box.conf[0] * 100)
+            cls = int(box.cls[0])
+            label = f"{CLASSES[cls]} {confidence}%"
+            (w, h), _ = cv2.getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.7, 1)
+            x1, y1, x2, y2 = x1, y1 - 20, x1 + w, y1
+            rectangle(frame, (x1, y1), (x2 + 10, y2 - 10), (223, 65, 80), FILLED)
+            putText(frame, label, (x1 + 5, y1 - 5), FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
 
-    def __init__(self):
-        self.stream = VideoCapture(0)
-        self.grabbed, self.frame = self.stream.read()
-        self.stopped = False
+    return frame
 
-    def start(self):
-        Thread(target=self.update, args=()).start()
-        return self
-
-    def update(self):
-        while True:
-            if self.stopped:
-                self.stream.release()
-                return
-            self.grabbed, self.frame = self.stream.read()
-
-    def read(self):
-        return self.frame
-
-    def stop(self):
-        self.stopped = True
-
-
-COLOR_RECTANGLE = (223, 65, 80)[::-1]
-COLOR_TEXT = (255, 255, 255)[::-1]
+def open_camera_window(window_name, frame):
+    cv2.imshow(window_name, frame)
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -62,91 +48,61 @@ parser.add_argument(
     help="where the parameters (JSON) file is located",
     default="params.json",
 )
-parser.add_argument(
-    "--source",
-    help="camera/webcam source",
-    default=0,
-)
 
 args = parser.parse_args()
 PARAMS_PATH = args.params
-SOURCE = args.source
 
 with open(PARAMS_PATH, "r") as file:
     params = load(file)
+
 MODEL = YOLO(params["model"])
 with open(params["coco"], "r") as file:
     CLASSES = file.read().rstrip("\n").split("\n")
 
-frame_rate_calc = 1
-freq = getTickFrequency()
-videostream = VideoStream().start()
-sleep(1)
-
-# Socket Create
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-print('HOST IP:', host_ip)
+host_ip = '192.168.43.95'  # Replace with your server's IP address
 port = 9999
-socket_address = (host_ip, port)
-
-# Socket Bind
-server_socket.bind(socket_address)
-
-# Socket Listen
+server_socket.bind((host_ip, port))
 server_socket.listen(5)
-print("LISTENING AT:", socket_address)
+print("Listening for incoming connections...")
 
-# Socket Accept
 while True:
     client_socket, addr = server_socket.accept()
-    print('GOT CONNECTION FROM:', addr)
-    if client_socket:
-        while True:
-            t1 = getTickCount()
-            frame1 = videostream.read()
-            frame = frame1.copy()
-            frame_rgb = cvtColor(frame, COLOR_BGR2RGB)
+    print('Received connection from', addr)
 
-            results = MODEL(frame_rgb, stream=True)
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4 * 1024)
+            if not packet:
+                break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-            for result in results:
-                boxes = result.boxes
-                for box in boxes:
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                    rectangle(frame, (x1, y1), (x2, y2), color=COLOR_RECTANGLE, thickness=2)
-                    confidence = ceil((box.conf[0] * 100)) / 100
-                    cls = int(box.cls[0])
-                    ox, oy = max(0, x1), max(35, y1)
-                    label = f"{CLASSES[cls]} {int(confidence*100)}%"
-                    (w, h), _ = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.7, 1)
-                    x1, y1, x2, y2 = ox, oy, ox + w, oy - h
-                    rectangle(frame, (x1, y1), (x2 + 10, y2 - 10), COLOR_RECTANGLE, FILLED)
-                    putText(
-                        frame, label, (ox + 5, oy - 5), FONT_HERSHEY_SIMPLEX, 0.7, COLOR_TEXT, 1
-                    )
+        while len(data) < msg_size:
+            data += client_socket.recv(4 * 1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
 
-            putText(
-                frame,
-                "FPS: {0:.2f}".format(frame_rate_calc),
-                (30, 50),
-                FONT_HERSHEY_SIMPLEX,
-                1,
-                (223, 65, 80)[::-1],
-                2,
-                LINE_AA,
-            )
+        # Perform object detection and annotate the frame
+        annotated_frame = object_detection(frame, MODEL)
 
-            imshow("Object detector", frame)
-            t2 = getTickCount()
-            time1 = (t2 - t1) / freq
-            frame_rate_calc = 1 / time1
+        # Display the annotated frame locally
+        open_camera_window("Server Camera", annotated_frame)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        client_socket.close()
-        destroyAllWindows()
-        videostream.stop()
-        break
+        # Send the annotated frame back to the client
+        annotated_data = pickle.dumps(annotated_frame)
+        client_socket.sendall(struct.pack("Q", len(annotated_data)) + annotated_data)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+    client_socket.close()
+
+server_socket.close()
+destroyAllWindows()
