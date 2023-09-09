8f4072464ee8fa444187fec849c67f6c0afeffc3
Aakash391 <108621987+Aakash391@users.noreply.github.com>
2023-09-09 23:33:46 +0530

    Update client.py
    Video capturing at Client

--- socket_prog/client.py
+++ socket_prog/client.py
@@ -1,27 +1,45 @@
-import socket,cv2, pickle,struct
+import socket
+import cv2
+import pickle
+import struct
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.43.95' # paste your server ip address here
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '192.168.43.95'  # Replace with your server's IP address
 port = 9999
-client_socket.connect((host_ip,port)) # a tuple
-data = b""
-payload_size = struct.calcsize("Q")
+client_socket.connect((host_ip, port))
+
+camera = cv2.VideoCapture(0)
+
+# Create a window for displaying the camera feed
+cv2.namedWindow("Client Camera", cv2.WINDOW_NORMAL)
+
 while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key  == ord('q'):
-		break
+    try:
+        # Capture a frame from the webcam
+        ret, frame = camera.read()
+        frame = cv2.resize(frame, (640, 480))
+
+        # Serialize the frame
+        data = pickle.dumps(frame)
+
+        # Send the frame size and then the frame data
+        client_socket.sendall(struct.pack("Q", len(data)) + data)
+
+        # Receive the annotated frame from the server (if needed)
+        # annotated_frame_data = client_socket.recv(4 * 1024)
+        # annotated_frame = pickle.loads(annotated_frame_data)
+
+        # Display the camera frame locally
+        cv2.imshow("Client Camera", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+    except Exception as e:
+        print(str(e))
+        break
+
+camera.release()
+cv2.destroyAllWindows()
 client_socket.close()
